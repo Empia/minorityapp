5b6082a9fb9c764b27a57f652be6f7200bcf6894
Stan <iamjacke@gmail.com>
2014-11-22 06:11:20 +0600

    Updated

--- Gemfile
+++ Gemfile
@@ -1,9 +1,14 @@
+# encoding: UTF-8
+Encoding.default_external = Encoding::UTF_8
+
 source 'https://rubygems.org'
 
-ruby '2.0.0'
+ruby '2.1.3'
 
-gem 'middleman', '~> 3.2'
-gem 'puma',      '~> 2.7'
+gem 'middleman', '~> 3.3.7'
+#gem 'puma',      '~> 2.7'
 gem 'rake',      '~> 10.1'
+
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 
 #gem 'wdm', '>= 0.1.0'# if RbConfig::CONFIG['target_os'] =~ /mswin|mingw/i
--- config.rb
+++ config.rb
@@ -1,5 +1,25 @@
+require 'net/http'
+
+class MyFeature < Middleman::Extension
+  def initialize(app, options_hash={}, &block)
+    super
+  end
+
+  helpers do
+    def minority(url, text)
+      Net::HTTP.get('run.plnkr.co', '/plunks/SHPgFo5HOs5G8uNukhBM/')
+    end
+  end
+  
+end
+
+
+::Middleman::Extensions.register(:my_feature, MyFeature)
+
 activate :automatic_image_sizes
 activate :directory_indexes
+activate :i18n
+activate :my_feature
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
@@ -9,6 +29,9 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
 end
+
+Encoding.default_internal = Encoding::UTF_8
+Encoding.default_external = Encoding::UTF_8
 
 # silence i18n warning
 ::I18n.config.enforce_available_locales = false
